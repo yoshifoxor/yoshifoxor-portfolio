4d04d9a80b597e801885a14cc5a584ebd1a588af
yoshifoxor <yshs.adachi@gmail.com>
2025-12-11 14:38:31 +0900

    feat: tsParticles を使用した背景パーティクルコンポーネントを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 export default function Home() {
   return (
-    <div className={`
-      flex min-h-screen items-center justify-center bg-zinc-50 font-sans
-      dark:bg-black
-    `}>
-      <main className={`
-        flex min-h-screen w-full max-w-3xl flex-col items-center justify-between
-        bg-white px-16 py-32
+    <div
+      className={`
+        m-auto flex w-full max-w-3xl flex-col justify-between bg-white px-16
+        py-32 align-middle
         sm:items-start
         dark:bg-black
-      `}>
-        <h1 className="text-5xl font-bold text-zinc-900 dark:text-zinc-100">
-          Welcome to Yoshifoxor&apos;s Portfolio!
-        </h1>
-        <p className="mt-4 text-2xl text-zinc-700 dark:text-zinc-300">
-          Explore my projects and experiences in software development.
-        </p>
-      </main>
+      `}
+    >
+      <h1 className="text-5xl font-bold text-zinc-900 dark:text-zinc-100">
+        Welcome to Yoshifoxor&apos;s Portfolio!
+      </h1>
+      <p className="mt-4 text-2xl text-zinc-700 dark:text-zinc-300">
+        Explore my projects and experiences in software development.
+      </p>
     </div>
   );
 }
